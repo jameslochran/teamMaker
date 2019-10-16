7a415f9a279679e54011164d0a3ca3b1821a6165
jameslochran <jlochran@gmail.com>
2019-10-15 22:10:58 -0400

    Fixed url

--- TeamMaker/urls.py
+++ TeamMaker/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('team/', include('team.urls')),
+    path('', include('team.urls')),
     path('admin/', admin.site.urls),
 ]
--- team/urls.py
+++ team/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('player/create', views.CreatePlayer.as_view(), name='create_player'),
     path('player/<int:pk>/add_skill', views.add_Skill, name='add_skill'),
     path('player/<int:pk>', views.DetailPlayer.as_view(), name='detail'),
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('teams', views.Teams, name='teams'),
 ]
