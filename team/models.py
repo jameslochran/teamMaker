044eb665d1bcb34a67e65f282a9bc533d755a6b4
jameslochran <jlochran@gmail.com>
2019-10-16 21:56:59 -0400

    updated procfile

--- team/models.py
+++ team/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
 
+#This is a test of the migration
 class Player(models.Model):
     name = models.CharField(max_length=255)
 
